9f13231c277896e45681b876b1b93aae9ffa3625
Ely A Ndiaye <ely.assumpcao.ndiaye@accenture.com>
2020-04-21 17:50:08 -0300

    Adiciona Nagivation

---
+++ TimeLine/Datasources/ItemSelectionDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  ItemSelectionDelegate.swift
+//  TimeLine
+//
+//  Created by ely.assumpcao.ndiaye on 21/04/20.
+//  Copyright © 2020 ely.assumpcao.ndiaye. All rights reserved.
+//
+
+import Foundation
--- TimeLine/SceneDelegate.swift
+++ TimeLine/SceneDelegate.swift
@@ -19,7 +19,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
-        window?.rootViewController = ReleaseViewController()
+        
+        let releaseVC = ReleaseViewController()
+        releaseVC.title = "TimeLine"
+        
+        let navController = UINavigationController(rootViewController: releaseVC)
+        
+        window?.rootViewController = navController
         window?.makeKeyAndVisible()
     }
 
